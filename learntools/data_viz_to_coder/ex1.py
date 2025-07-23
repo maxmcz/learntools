5f9df167096391e04cf8f53becca8ddd71b33180
Maxwell G. de Oliveira <maxmcz@gmail.com>
2025-07-23 11:59:35 -0300

    Update ex1.py

--- learntools/data_viz_to_coder/ex1.py
+++ learntools/data_viz_to_coder/ex1.py
@@ -10,7 +10,7 @@ df = pd.read_csv("../input/fifa.csv", index_col="Date", parse_dates=True)
 class FeedbackSys(EqualityCheckProblem):
     _var = 'one'
     _expected = 1
-    _hint = ("How many moons does Earth have?")
+    _hint = ("Quantas luas a Terra tem?")
     _solution = CS('one = 1')
     
 class LoadFIFAData(EqualityCheckProblem):
@@ -25,8 +25,8 @@ class LoadFIFAData(EqualityCheckProblem):
 
 class PlotLine(CodingProblem):
     _var = 'plt'
-    _hint = ("Refer to the tutorial to see the solution.  The line of code that you need "
-             " to fill in begins with `sns.lineplot`.")
+    _hint = ("Consulte o tutorial para ver a solução. A linha de código que você precisa "
+             " para preencher começa com `sns.lineplot`.")
     _solution = CS(
 """# Set the width and height of the figure
 plt.figure(figsize=(16,6))
@@ -43,26 +43,26 @@ sns.lineplot(data=fifa_data)
     def check(self, passed_plt):
         
         assert len(passed_plt.figure(1).axes) > 0, \
-        ("After you've written code to create a line chart, `check()` will tell "
-         "you whether your code is correct.")
+        ("Depois de escrever o código para criar um gráfico de linhas, `check()` vai revelar "
+         "se seu código está correto.")
         
         main_axis = passed_plt.figure(1).axes[0]
         legend_handles = main_axis.get_legend_handles_labels()[0]
         
         assert all(isinstance(x, matplotlib.lines.Line2D) for x in legend_handles), \
-        ("Is your figure a line chart?  Please use `sns.lineplot()` to generate "
-         "the lines in your figure.")
+        ("A sua figura é um gráfico de linhas? Por favor, use `sns.lineplot()` para gerar "
+         "as linhas na sua figura.")
 
         assert len(legend_handles) == 6, \
-        ("Your plot does not seem to have 6 lines (one line for each museum). "
-         "We detect %d lines.") % len(legend_handles)
+        ("Sua plotagem não parece ter 6 linhas (uma linha para cada museu). "
+         "Consegui detectar %d linhas.") % len(legend_handles)
         
 class ThinkLine(ThoughtExperiment):
-    _hint = ("Which lines stay at least five consecutive years at the bottom of the chart?")
-    _solution = ("The only country that meets this criterion is Brazil (code: BRA), as it "
-                 "maintains the highest ranking in 1996-2000.  Other countries do spend some "
-                 "time in the number 1 spot, but Brazil is the only country that maintains it "
-                 "for at least five **consecutive** years.")
+    _hint = ("Quais linhas permanecem pelo menos cinco anos consecutivos na parte inferior do gráfico?")
+    _solution = ("O único país que atende a esse critério é o Brasil (code: BRA), mantendo "
+                 "a posição mais alta entre 1996 e 2000. Outros países permanecem na primeira posição "
+                 "por algum tempo, mas o Brasil é o único que a mantém por pelo menos cinco anos "
+                 "**consecutivos**.")
         
 Line = MultipartProblem(PlotLine, ThinkLine)
        
